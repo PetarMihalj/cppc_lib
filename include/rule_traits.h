992903c3d24eb9f84686837bb212f5fd8d73c728
Petar Mihalj <git@pmihalj.com>
2021-09-04 12:39:24 +0200

    Add better rule traits (no recursion)

--- include/rule_traits.h
+++ include/rule_traits.h
@@ -3,78 +3,71 @@
 namespace rule_traits {
 
 namespace internals {
-template <typename T> struct negation { static const bool value = true; };
 
-template <> struct negation<std::true_type> {
-    static const bool value = false;
-};
+template<typename T>
+using negation = typename std::conditional<T::value, std::false_type, std::true_type>::type;
 
-template <typename T> struct type_to_value { static const bool value = true; };
+template<typename ...T> struct bool_pack {};
 
-template <> struct type_to_value<std::false_type> {
-    static const bool value = false;
-};
+template<typename ...T>
+using conjunction = typename std::is_same<bool_pack<std::true_type, T...>, bool_pack<T..., std::true_type>>::type;
 
-template <typename... T> struct conjunction { static const bool value = true; };
-
-template <typename T1, typename... T> struct conjunction<T1, T...> {
-    static const bool value = type_to_value<
-        std::conditional<T1::value, conjunction<T...>, std::false_type>>::value;
-};
 } // namespace internals
 
+
+
 template <typename T>
-using is_zero = internals::conjunction<std::is_trivially_destructible<T>,
-                                       std::is_trivially_constructible<T>,
-                                       std::is_trivially_copy_constructible<T>,
-                                       std::is_trivially_copy_assignable<T>,
-                                       std::is_trivially_move_constructible<T>,
-                                       std::is_trivially_move_assignable<T>>;
+using is_zero = internals::conjunction<typename std::is_trivially_destructible<T>::type,
+                                       typename std::is_trivially_constructible<T>::type,
+                                       typename std::is_trivially_copy_constructible<T>::type,
+                                       typename std::is_trivially_copy_assignable<T>::type,
+                                       typename std::is_trivially_move_constructible<T>::type,
+                                       typename std::is_trivially_move_assignable<T>::type>;
 
 template <typename T>
 using is_three_copy = internals::conjunction<
-    internals::negation<std::is_trivially_destructible<T>>,
-    internals::negation<std::is_trivially_constructible<T>>,
-    internals::negation<std::is_trivially_copy_constructible<T>>,
-    internals::negation<std::is_trivially_copy_assignable<T>>,
+    internals::negation<typename std::is_trivially_destructible<T>::type>,
+    internals::negation<typename std::is_trivially_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_copy_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_copy_assignable<T>::type>,
 
-    std::is_destructible<T>, std::is_copy_constructible<T>,
-    std::is_copy_assignable<T>,
+    typename std::is_destructible<T>::type, typename std::is_copy_constructible<T>::type,
+    typename std::is_copy_assignable<T>::type,
 
-    internals::negation<std::is_move_constructible<T>>,
-    internals::negation<std::is_move_assignable<T>>>;
+    internals::negation<typename std::is_move_constructible<T>::type>,
+    internals::negation<typename std::is_move_assignable<T>::type>>;
 
 template <typename T>
 using is_three_move = internals::conjunction<
-    internals::negation<std::is_trivially_destructible<T>>,
-    internals::negation<std::is_trivially_constructible<T>>,
-    internals::negation<std::is_trivially_move_constructible<T>>,
-    internals::negation<std::is_trivially_move_assignable<T>>,
+    internals::negation<typename std::is_trivially_destructible<T>::type>,
+    internals::negation<typename std::is_trivially_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_move_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_move_assignable<T>::type>,
 
-    std::is_destructible<T>, std::is_move_constructible<T>,
-    std::is_move_assignable<T>,
+    typename std::is_destructible<T>::type, typename std::is_move_constructible<T>::type,
+    typename std::is_move_assignable<T>::type,
 
-    internals::negation<std::is_copy_constructible<T>>,
-    internals::negation<std::is_copy_assignable<T>>>;
+    internals::negation<typename std::is_copy_constructible<T>::type>,
+    internals::negation<typename std::is_copy_assignable<T>::type>>;
 
 template <typename T>
 using is_five = internals::conjunction<
-    internals::negation<std::is_trivially_destructible<T>>,
-    internals::negation<std::is_trivially_constructible<T>>,
-    internals::negation<std::is_trivially_copy_constructible<T>>,
-    internals::negation<std::is_trivially_copy_assignable<T>>,
-    internals::negation<std::is_trivially_move_constructible<T>>,
-    internals::negation<std::is_trivially_move_assignable<T>>,
+    internals::negation<typename std::is_trivially_destructible<T>::type>,
+    internals::negation<typename std::is_trivially_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_copy_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_copy_assignable<T>::type>,
+    internals::negation<typename std::is_trivially_move_constructible<T>::type>,
+    internals::negation<typename std::is_trivially_move_assignable<T>::type>,
 
-    std::is_destructible<T>, std::is_copy_constructible<T>,
-    std::is_copy_assignable<T>, std::is_move_constructible<T>,
-    std::is_move_assignable<T>>;
+    typename std::is_destructible<T>::type, typename std::is_copy_constructible<T>::type,
+    typename std::is_copy_assignable<T>::type, typename std::is_move_constructible<T>::type,
+    typename std::is_move_assignable<T>::type>;
 
 template <typename T>
 using is_no_copy_move =
-    internals::conjunction<internals::negation<std::is_copy_constructible<T>>,
-                           internals::negation<std::is_copy_assignable<T>>,
-                           internals::negation<std::is_move_constructible<T>>,
-                           internals::negation<std::is_move_assignable<T>>>;
+    internals::conjunction<internals::negation<typename std::is_copy_constructible<T>::type>,
+                           internals::negation<typename std::is_copy_assignable<T>::type>,
+                           internals::negation<typename std::is_move_constructible<T>::type>,
+                           internals::negation<typename std::is_move_assignable<T>::type>>;
 
 } // namespace rule_traits
